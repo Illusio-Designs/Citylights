5a820b72aa99da337d32fefd198da7aac1731459
Mansi Fichadiya <mansifichadiya7@gmail.com>
2025-05-24 18:27:40 +0530

    Refactor App component to render Home page instead of Header. Implement Home component with hero section, about us content, and product icons for enhanced user experience.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import Header from "./component/Header";
-
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Header />
+      <Home />
     </>
   );
 }
