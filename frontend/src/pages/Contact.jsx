c631ce4d76ac5922f087635394d67d67a278dea8
RiyaLunagariya <149014724+RiyaLunagariya@users.noreply.github.com>
2025-06-07 15:32:47 +0530

    Add Google Maps integration to Contact page
    - Introduced a map container on the Contact page featuring an embedded Google Maps iframe for location display.
    - Updated Contact.css to style the map container and iframe for improved aesthetics and responsiveness.

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -52,6 +52,17 @@ const Contact = () => {
           </div>
         </form>
       </div>
+      <div className="map-container">
+        <iframe 
+          src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d65548.28468861307!2d70.7933990493785!3d22.283261846539638!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1749289961835!5m2!1sen!2sin" 
+          width="600" 
+          height="450" 
+          style={{border: 0}} 
+          allowFullScreen="" 
+          loading="lazy" 
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
     </div>
     <Footer />
     </>
